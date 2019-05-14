af68138d7d60adb079811011ac473d90d64e5d75
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-05-13 20:29:54 -0400

    Delete setup.sql

--- general_journal/setup.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS general_journal;
-CREATE DATABASE general_journal;
-
-DROP USER IF EXISTS general_journal;
-CREATE USER general_journal WITH PASSWORD 'general_journal';
-
-GRANT ALL PRIVILEGES ON DATABASE general_journal TO general_journal;
-GRANT postgres TO general_journal;
