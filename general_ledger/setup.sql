e09f5b64c02d0eca3b6bda682d26a0b57e47f78f
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-05-13 20:30:07 -0400

    Delete setup.sql

--- general_ledger/setup.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS general_ledger;
-CREATE DATABASE general_ledger;
-
-DROP USER IF EXISTS general_ledger;
-CREATE USER general_ledger WITH PASSWORD 'general_ledger';
-
-GRANT ALL PRIVILEGES ON DATABASE general_ledger TO general_ledger;
-GRANT postgres TO general_ledger;
