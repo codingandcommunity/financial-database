d94fd80e2e9babd2beedb099575178cc6282e7de
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-05-13 20:31:58 -0400

    Delete setup.sql

--- subsidiary_ledger/Cash/setup.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS cash_ledger;
-CREATE DATABASE cash_ledger;
-
-DROP USER IF EXISTS;
-CREATE USER cash_ledger WITH PASSWORD 'cash_ledger';
-
-GRANT ALL PRIVILEGES ON DATABASE cash_ledger TO cash_ledger;
-GRANT postgres TO cash_ledger;
