db90657ceb2968794d7cfe422b78740dccf01f6d
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-05-13 20:31:47 -0400

    Delete setup.sql

--- subsidiary_ledger/AR/setup.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS ar_ledger;
-CREATE DATABASE ar_ledger;
-
-DROP USER IF EXISTS ar_ledger;
-CREATE USER ar_ledger WITH PASSWORD 'ar_ledger';
-
-GRANT ALL PRIVILEGES ON DATABASE ar_ledger TO ar_ledger;
-GRANT postgres TO ar_ledger;
