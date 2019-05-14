f77d5c4a2d3301aa80eab371e74ed48e0597d677
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-05-13 20:30:33 -0400

    Delete setup.sql

--- subsidiary_ledger/AP/setup.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS ap_ledger;
-CREATE DATABASE ap_ledger;
-
-DROP USER IF EXISTS ap_ledger;
-CREATE USER ap_ledger WITH PASSWORD 'ap_ledger';
-
-GRANT ALL PRIVILEGES ON DATABASE ap_ledger TO ap_ledger;
-GRANT postgres TO ap_ledger;
